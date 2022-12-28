a33ed111df7aec25c0325c36bd07c421fb5f0509
Ashtone Onyango <50997863+ASHTONEONYANGO@users.noreply.github.com>
2022-12-29 02:33:15 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,224 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+  /**
+   * Sticky header on scroll
+   */
+  const selectHeader = document.querySelector('#header');
+  if (selectHeader) {
+    document.addEventListener('scroll', () => {
+      window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+    });
+  }
+
+  /**
+   * Scroll top button
+   */
+  const scrollTop = document.querySelector('.scroll-top');
+  if (scrollTop) {
+    const togglescrollTop = function() {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+    window.addEventListener('load', togglescrollTop);
+    document.addEventListener('scroll', togglescrollTop);
+    scrollTop.addEventListener('click', window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    }));
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  const mobileNavShow = document.querySelector('.mobile-nav-show');
+  const mobileNavHide = document.querySelector('.mobile-nav-hide');
+
+  document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
+    el.addEventListener('click', function(event) {
+      event.preventDefault();
+      mobileNavToogle();
+    })
+  });
+
+  function mobileNavToogle() {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    mobileNavShow.classList.toggle('d-none');
+    mobileNavHide.classList.toggle('d-none');
+  }
+
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#navbar a').forEach(navbarlink => {
+
+    if (!navbarlink.hash) return;
+
+    let section = document.querySelector(navbarlink.hash);
+    if (!section) return;
+
+    navbarlink.addEventListener('click', () => {
+      if (document.querySelector('.mobile-nav-active')) {
+        mobileNavToogle();
+      }
+    });
+
+  });
+
+  /**
+   * Toggle mobile nav dropdowns
+   */
+  const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+
+  navDropdowns.forEach(el => {
+    el.addEventListener('click', function(event) {
+      if (document.querySelector('.mobile-nav-active')) {
+        event.preventDefault();
+        this.classList.toggle('active');
+        this.nextElementSibling.classList.toggle('dropdown-active');
+
+        let dropDownIndicator = this.querySelector('.dropdown-indicator');
+        dropDownIndicator.classList.toggle('bi-chevron-up');
+        dropDownIndicator.classList.toggle('bi-chevron-down');
+      }
+    })
+  });
+
+  /**
+   * Initiate pURE cOUNTER
+   */
+  new PureCounter();
+
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Init swiper slider with 1 slide at once in desktop view
+   */
+  new Swiper('.slides-1', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
+
+});
+
+// Carousel
+// Initialize variables
+const carousel = document.querySelector('.carousel');
+const images = document.querySelectorAll('.carousel img');
+const leftControl = document.querySelector('.controls.left');
+const rightControl = document.querySelector('.controls.right');
+const indicators = document.querySelectorAll('.indicators button');
+let currentIndex = 0;
+
+// Function to go to the previous image
+function previousImage() {
+  // Decrement the current index
+  currentIndex--;
+
+  // If the current index is negative, set it to the last image
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+
+  // Update the carousel
+  updateCarousel();
+}
+
+// Function to go to the next image
+function nextImage() {
+  // Increment the current index
+  currentIndex++;
+
+  // If the current index is greater than the number of images, set it to the first image
+  if (currentIndex > images.length - 1) {
+    currentIndex = 0;
+  }
+
+  // Update the carousel
+  updateCarousel();
+}
+
+// Function to update the carousel
+function updateCarousel() {
+  // Set the src of the current image to the src of the selected image
+  carousel.src = images[currentIndex].src;
+
+  // Set the active indicator
+  indicators.forEach((indicator, index) => {
+    if (index === currentIndex) {
+      indicator.classList.add('active');
+    } else {
+      indicator.classList.remove('active');
+    }
+  });
+}
+
+// Function to start the automatic sliding
+function startSlideshow() {
+  // Set a timeout to call the nextImage function after 5 seconds
+  setTimeout(() => {
+    nextImage();
+
+    // Restart the timeout
+    startSlideshow();
+  }, 5000);
+}
+
+// Add event listeners for the controls
+leftControl.addEventListener('click', previousImage);
+rightControl.addEventListener('click', nextImage);
+
+// Add event listeners for the indicators
+indicators.forEach((indicator, index) => {
+  indicator.addEventListener('click', () => {
+    currentIndex = index;
+    updateCarousel();
+  });
+});
+
+// Start the automatic sliding
+startSlideshow();
